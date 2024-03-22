2fffd11fe8dc6d897a86ed66c012efa6d289f8b7
Jay <merqqcury@gmail.com>
2024-03-22 15:06:56 +0800

    Add __ffs function
    __ffs is a binary saerch way to find MSB bit

--- week4/sqrt.c
+++ week4/sqrt.c
@@ -10,8 +10,9 @@ int i_sqrt3(int n);
 int i_sqrt4(int n);
 int i_sqrt5(int n);
 
-func_t f = i_sqrt4;
+func_t f = i_sqrt5;
 int n = 0;
+static inline unsigned long __ffs(int word);
 int main(int argc, char *argv[]){
     if(argc == 1){
         printf("Usage: %s <number>\n", argv[0]);
@@ -87,17 +88,48 @@ int i_sqrt4(int n)
     return c;
 }
 
+// replace ffs with __ffs
 int i_sqrt5(int n)
 {
     if (n <= 1) /* Assume n is always positive */
         return n;
 
     int c = 0;
-    for (int d = 1UL << ((31 - __builtin_clz(n)) & ~1UL); d; d >>= 2) {
+    for (int d = 1UL << ((31 - __ffs(n)) & ~1UL); d; d >>= 2) {
         int y = c + d;
         c >>= 1;
         if (n >= y)
             n -= y, c += d;               
     }
     return c;
+}
+static inline unsigned long __ffs(int word)
+{
+    int num = 0;
+
+#if BITS_PER_LONG == 64
+    if ((word & 0xffffffff) == 0) {
+        num += 32;
+        word >>= 32;
+    }
+#endif
+    if ((word & 0xffff) == 0) {
+        num += 16;
+        word >>= 16;
+    }
+    if ((word & 0xff) == 0) {
+        num += 8;
+        word >>= 8;
+    }
+    if ((word & 0xf) == 0) {
+        num += 4;
+        word >>= 4;
+    }
+    if ((word & 0x3) == 0) {
+        num += 2;
+        word >>= 2;
+    }
+    if ((word & 0x1) == 0)
+        num += 1;
+    return num;
 }
